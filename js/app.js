7e5817b4b36f0bd880ab61614cc53ae5618db13c
Jimmy Guzman <jimmy.guzman@outlook.com>
2018-01-22 19:10:31 -0600

    rename file, reformatted code

--- js/app.js
+++ js/app.js
@@ -1,23 +1,39 @@
 // API-URL
-var url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=";
+var url =
+  "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=";
 
 $(document).ready(function() {
-  $(".search-box").css("margin-top", ((window.innerHeight - $(".container").height()) * 0.35));
+  $(".search-box").css(
+    "margin-top",
+    (window.innerHeight - $(".container").height()) * 0.35
+  );
 
   function getResults() {
-    $(".search-box").animate({
-      "margin-top": 0
-    }, 500);
+    $(".search-box").animate(
+      {
+        "margin-top": 0
+      },
+      500
+    );
     $("#results").text("");
 
     $.getJSON(url + $("#search").val() + "&origin=*", function(data) {
-
       for (var i = 0; i < data[1].length; i++) {
-        $("#results").hide().append('<a href="' + data[3][i] + '" target="_blank"><li><p class="caption">' + data[1][i] + '</p><p class="excerpt">' + data[2][i] + '</p></li></a>').slideDown();
+        $("#results")
+          .hide()
+          .append(
+            '<a href="' +
+              data[3][i] +
+              '" target="_blank"><li><p class="caption">' +
+              data[1][i] +
+              '</p><p class="excerpt">' +
+              data[2][i] +
+              "</p></li></a>"
+          )
+          .slideDown();
       }
-
     });
-  };
+  }
 
   $("#btn-submit").click(function() {
     getResults();
@@ -33,12 +49,8 @@ $(document).ready(function() {
     }
   });
 
-
   //random button opens up random wikipedia page on new tab
   $("#btn-random").click(function() {
     window.open("https://en.wikipedia.org/wiki/Special:Random", "_blank");
   });
-
-
-
 });
